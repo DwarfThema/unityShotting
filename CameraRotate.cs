ea59f7492530aa994657a014e2a9352a4eab3228
DwarfThema <86091261+DwarfThema@users.noreply.github.com>
2022-05-24 22:08:48 +0900

    Add project files.

---
+++ CameraRotate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    float rx;
+    float ry;
+
+    public float rotSpeed = 200;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mx = Input.GetAxis("Mouse X");
+        float my = Input.GetAxis("Mouse Y");
+
+        rx += rotSpeed * my * Time.deltaTime;
+        ry += rotSpeed * mx * Time.deltaTime;
+
+        rx = Mathf.Clamp(rx, -80, 80);
+
+        transform.eulerAngles = new Vector3(-rx, ry, 0);
+    }
+}
---
+++ PlayerMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 5;
+    public float gravity = -9.81f;
+    public float jumpPower = 10;
+    float yVelocity;
+
+    CharacterController cc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yVelocity += gravity * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            yVelocity = jumpPower;
+        }
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = Vector3.right * h + Vector3.forward * v;
+
+        dir.Normalize();
+
+        dir.y = yVelocity;
+
+        cc.Move(dir * speed * Time.deltaTime);
+
+    }
+}
